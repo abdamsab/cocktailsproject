97d692d6a29a0f059347257e22d78da6b7a5fe53
abdamsab <abdamsabinfo@gmail.com>
2022-01-25 11:30:02 +0000

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section" >
+      <h1 className="section-title" >about us</h1>
+
+			<p>
+				Agreed joy vanity regret met may ladies oppose who. Mile fail as left as hard eyes. 
+				Meet made call in mean four year it to. Prospect so branched wondered sensible of up. 
+				For gay consisted resolving pronounce sportsman saw discovery not. Northward or household 
+				as conveying we earnestly believing. No in up contrasted discretion inhabiting excellence. 
+				Entreaties we collecting unpleasant at everything conviction.
+			</p>
+
+    </section>
+  )
+}
+
+export default About
